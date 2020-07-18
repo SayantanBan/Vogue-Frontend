8dfd9aedce73fd96471c665406e6a374819d380a
Sayantan <sayantanbanerjee@outlook.com>
2020-07-19 00:38:00 +0530

    facebook plugin fix

--- src/app/Modules/home-page/post-detail/post-detail.component.ts
+++ src/app/Modules/home-page/post-detail/post-detail.component.ts
@@ -1,28 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { PostService } from 'src/app/Shared/Services/post.service';
-import { SafeUrl, DomSanitizer } from '@angular/platform-browser';
-import { Post } from 'src/app/Shared/Models/Post';
-import { DataStorageService } from '../service/data-storage.service';
-import { NgxSpinnerService } from 'ngx-spinner';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { PostService } from "src/app/Shared/Services/post.service";
+import { SafeUrl, DomSanitizer } from "@angular/platform-browser";
+import { Post } from "src/app/Shared/Models/Post";
+import { DataStorageService } from "../service/data-storage.service";
+import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
-  selector: 'app-post-detail',
-  templateUrl: './post-detail.component.html',
-  styleUrls: ['./post-detail.component.scss']
+  selector: "app-post-detail",
+  templateUrl: "./post-detail.component.html",
+  styleUrls: ["./post-detail.component.scss"],
 })
 export class PostDetailComponent implements OnInit {
-
-  fbCommentPluginLink: string
+  fbCommentPluginLink: string;
   fbUrl: SafeUrl;
-  script: any = 'javascript:FB.XFBML.parse()';
+  script: any = "javascript:FB.XFBML.parse()";
   trustedScript: any;
   post: Post;
-  constructor(private route: ActivatedRoute,
+  constructor(
+    private route: ActivatedRoute,
     private service: PostService,
     private dataStorageService: DataStorageService,
     private sanitizer: DomSanitizer,
-    private spinner: NgxSpinnerService) {
+    private spinner: NgxSpinnerService
+  ) {
     this.spinner.show();
     this.loadAPI();
   }
@@ -30,23 +31,25 @@ export class PostDetailComponent implements OnInit {
   ngOnInit() {
     if (this.service.getPosts().length <= 0) {
       this.dataStorageService.fetchPosts().subscribe(
-        result => {
+        (result) => {
           this.dataStorageService.fetchCategories().subscribe(
-            result => {
+            (result) => {
               this.getPostDetail();
               this.spinner.hide();
             },
-            error => {
+            (error) => {
               console.error(
                 `Backend returned code ${error.status}, ` +
-                `body was: ${error.error}`);
+                  `body was: ${error.error}`
+              );
             }
           );
         },
-        error => {
+        (error) => {
           console.error(
             `Backend returned code ${error.status}, ` +
-            `body was: ${error.error}`);
+              `body was: ${error.error}`
+          );
         }
       );
     } else {
@@ -57,19 +60,23 @@ export class PostDetailComponent implements OnInit {
 
   loadAPI() {
     this.trustedScript = this.sanitizer.bypassSecurityTrustUrl(this.script);
-    var js1 = document.createElement('script');
-    js1.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.3&appId=616949518696820&autoLogAppEvents=1';
+    var js1 = document.createElement("script");
+    js1.src =
+      "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.3&appId=616949518696820&autoLogAppEvents=1";
     document.body.appendChild(js1);
-    var js2 = document.createElement('script');
-    js2.src = '../assets/js/XFBML.js';
+    var js2 = document.createElement("script");
+    js2.src = "../assets/js/XFBML.js";
     document.body.appendChild(js2);
   }
 
   getPostDetail() {
-    const id = +this.route.snapshot.paramMap.get('id');
-    this.post = this.service.getUniquePost(id)
+    const id = +this.route.snapshot.paramMap.get("id");
+    this.post = this.service.getUniquePost(id);
     // this.service.getPost(id).subscribe(result => {console.log(result);this.post = result}, error => console.log(error));
-    this.fbCommentPluginLink = "http://localhost:4200/home/detail/" + id;
-    this.fbUrl = this.sanitizer.bypassSecurityTrustUrl(this.fbCommentPluginLink);
+    this.fbCommentPluginLink =
+      "https://vogue-dev.herokuapp.com/home/detail/" + id;
+    this.fbUrl = this.sanitizer.bypassSecurityTrustUrl(
+      this.fbCommentPluginLink
+    );
   }
 }
--- src/app/Modules/home-page/user-posts/user-posts.component.ts
+++ src/app/Modules/home-page/user-posts/user-posts.component.ts
@@ -1,159 +1,36 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { PostService } from "src/app/Shared/Services/post.service";
 import { UserService } from "src/app/Shared/Services/user.service";
 import { TOKEN_USER } from "src/app/Shared/Services/auth.constant";
 import { Post } from "src/app/Shared/Models/Post";
 import { NgxSpinnerService } from "ngx-spinner";
+import { DataStorageService } from "../service/data-storage.service";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: "app-user-posts",
   templateUrl: "./user-posts.component.html",
   styleUrls: ["./user-posts.component.scss"],
 })
-export class UserPostsComponent implements OnInit {
+export class UserPostsComponent implements OnInit, OnDestroy {
   p: number = 1;
   numbers: number[];
   allPosts: Post[];
   isLoading = true;
-  _value: string;
-  _titleSearch: string;
   sortedTitleSearch: boolean;
-  _postdateSearch: string;
-  _editdateSearch: string;
-  _categorySearch: string;
-  _statusSearch: string;
-  _commentsAlowedSearch: string;
   filteredPostList: Post[];
-
-  get value(): string {
-    // console.log("getter");
-    // console.log(this._listFilter);
-    return this._value;
-  }
-
-  set value(value: string) {
-    // console.log("setter");
-    this._value = value.toLowerCase();
-    this.filteredPostList = this.value
-      ? this.performFilter(this.value, 1)
-      : this.allPosts;
-  }
-
-  get titleSearch(): string {
-    return this._titleSearch;
-  }
-
-  set titleSearch(value: string) {
-    this._titleSearch = value.toLowerCase();
-    this.filteredPostList = this.titleSearch
-      ? this.performFilter(this.titleSearch, 2)
-      : this.allPosts;
-  }
-
-  get postdateSearch(): string {
-    return this._postdateSearch;
-  }
-
-  set postdateSearch(value: string) {
-    this._postdateSearch = value.toLowerCase();
-    this.filteredPostList = this.postdateSearch
-      ? this.performFilter(this.postdateSearch, 3)
-      : this.allPosts;
-  }
-
-  get statusSearch(): string {
-    return this._statusSearch;
-  }
-
-  set statusSearch(value: string) {
-    this._statusSearch = value.toLowerCase();
-    this.filteredPostList = this.statusSearch
-      ? this.performFilter(this.statusSearch, 4)
-      : this.allPosts;
-  }
-
-  get categorySearch(): string {
-    return this._categorySearch;
-  }
-
-  set categorySearch(value: string) {
-    this._categorySearch = value.toLowerCase();
-    this.filteredPostList = this.categorySearch
-      ? this.performFilter(this.categorySearch, 5)
-      : this.allPosts;
-  }
-
-  get commentsAlowedSearch(): string {
-    // console.log("getter");
-    // console.log(this._listFilter);
-    return this._value;
-  }
-
-  set commentsAlowedSearch(value: string) {
-    // console.log("setter");
-    this._commentsAlowedSearch = value.toLowerCase();
-    this.filteredPostList = this.commentsAlowedSearch
-      ? this.performFilter(this.commentsAlowedSearch, 6)
-      : this.allPosts;
-  }
-
-  get editdateSearch(): string {
-    // console.log("getter");
-    // console.log(this._listFilter);
-    return this._editdateSearch;
-  }
-
-  set editdateSearch(value: string) {
-    // console.log("setter");
-    this._editdateSearch = value.toLowerCase();
-    // this.filteredPostList = this.commentsAlowedSearch
-    //   ? this.performFilter(this.commentsAlowedSearch, 6)
-    //   : this.allPosts;
-  }
-
-  performFilter(filterBy: string, value: number): Post[] {
-    if (value == 1)
-      return this.filteredPostList.filter(
-        (post: Post) =>
-          post.postName.toLocaleLowerCase().indexOf(filterBy) !== -1
-      );
-    else if (value == 2)
-      return this.filteredPostList.filter(
-        (post: Post) =>
-          post.postName.toLocaleLowerCase().indexOf(filterBy) !== -1
-      );
-    else if (value == 3)
-      return this.filteredPostList.filter(
-        (post: Post) =>
-          String(post.postDate).toLocaleLowerCase().indexOf(filterBy) !== -1
-      );
-    else if (value == 4)
-      return this.filteredPostList.filter((post: Post) => {
-        console.log(String(post.status));
-        String(post.status).indexOf(filterBy) !== -1;
-      });
-    else if (value == 5)
-      return this.filteredPostList.filter(
-        (post: Post) =>
-          post.category.categoryName
-            .toString()
-            .toLocaleLowerCase()
-            .indexOf(filterBy) !== -1
-      );
-    else if (value == 5)
-      return this.filteredPostList.filter(
-        (post: Post) =>
-          post.allowComment.toString().toLocaleLowerCase().indexOf(filterBy) !==
-          -1
-      );
-  }
+  fetchPost: Subscription;
 
   constructor(
     private userService: UserService,
     private postService: PostService,
-    private spinner: NgxSpinnerService
+    private spinner: NgxSpinnerService,
+    private dataStorageService: DataStorageService
   ) {
     this.sortedTitleSearch = false;
+  }
+  ngOnDestroy(): void {
+    if (this.fetchPost != null) this.fetchPost.unsubscribe();
   }
 
   ngOnInit() {
@@ -174,6 +51,7 @@ export class UserPostsComponent implements OnInit {
               .map((x, i) => i);
             this.isLoading = false;
             this.spinner.hide();
+            this.fetchPost = this.dataStorageService.fetchPosts().subscribe();
           },
           (error) => console.log(error)
         ),
@@ -186,123 +64,9 @@ export class UserPostsComponent implements OnInit {
       (result) => {
         console.log(result);
         this.showUserPost();
+        this.fetchPost = this.dataStorageService.fetchPosts().subscribe();
       },
       (error) => console.log(error)
     );
-  }
-
-  onSort(column: number) {
-    switch (column) {
-      case 1:
-        if (this.sortedTitleSearch) {
-          console.log("Sorting Ascending:");
-          this.filteredPostList.sort((a, b) =>
-            a.postName > b.postName ? -1 : 1
-          );
-          this.sortedTitleSearch = false;
-        } else {
-          console.log("Sorting Descending:");
-          this.filteredPostList.sort((a, b) =>
-            a.postName < b.postName ? -1 : 1
-          );
-          this.sortedTitleSearch = true;
-        }
-        break;
-      case 2:
-        if (this.sortedTitleSearch) {
-          console.log("Sorting Ascending:");
-          this.filteredPostList.sort((a, b) =>
-            a.postDate > b.postDate ? -1 : 1
-          );
-          this.sortedTitleSearch = false;
-        } else {
-          console.log("Sorting Descending:");
-          this.filteredPostList.sort((a, b) =>
-            a.postDate < b.postDate ? -1 : 1
-          );
-          this.sortedTitleSearch = true;
-        }
-        break;
-      case 3:
-        if (this.sortedTitleSearch) {
-          console.log("Sorting Ascending:");
-          this.filteredPostList.sort((a, b) =>
-            a.lastEdit > b.lastEdit ? -1 : 1
-          );
-          this.sortedTitleSearch = false;
-        } else {
-          console.log("Sorting Descending:");
-          this.filteredPostList.sort((a, b) =>
-            a.lastEdit < b.lastEdit ? -1 : 1
-          );
-          this.sortedTitleSearch = true;
-        }
-        break;
-      case 4:
-        if (this.sortedTitleSearch) {
-          console.log("Sorting Ascending:");
-          this.filteredPostList.sort((a, b) =>
-            a.category.categoryName > b.category.categoryName ? -1 : 1
-          );
-          this.sortedTitleSearch = false;
-        } else {
-          console.log("Sorting Descending:");
-          this.filteredPostList.sort((a, b) =>
-            a.category.categoryName < b.category.categoryName ? -1 : 1
-          );
-          this.sortedTitleSearch = true;
-        }
-        break;
-      case 5:
-        if (this.sortedTitleSearch) {
-          console.log("Sorting Ascending:");
-          this.filteredPostList.sort((a, b) => (a.status > b.status ? -1 : 1));
-          this.sortedTitleSearch = false;
-        } else {
-          console.log("Sorting Descending:");
-          this.filteredPostList.sort((a, b) => (a.status < b.status ? -1 : 1));
-          this.sortedTitleSearch = true;
-        }
-        break;
-      case 6:
-        if (this.sortedTitleSearch) {
-          console.log("Sorting Ascending:");
-          this.filteredPostList.sort((a, b) =>
-            a.allowComment > b.allowComment ? -1 : 1
-          );
-          this.sortedTitleSearch = false;
-        } else {
-          console.log("Sorting Descending:");
-          this.filteredPostList.sort((a, b) =>
-            a.allowComment < b.allowComment ? -1 : 1
-          );
-          this.sortedTitleSearch = true;
-        }
-        break;
-    }
-  }
-
-  onPageChange(pageNumber: number) {
-    if (this.p == 1 && pageNumber == 1) {
-      console.log("firstPage...");
-      if (this.filteredPostList.length < 10) {
-        this.filteredPostList.slice(0, this.filteredPostList.length);
-      }
-    } else if (pageNumber + 1 == this.p) {
-      console.log("lastPage...");
-      if ((pageNumber + 1) * 10 + 10 > this.p) {
-        console.log(this.filteredPostList.slice(pageNumber * 10, this.p));
-        this.filteredPostList = this.filteredPostList.slice(
-          (pageNumber + 1) * 10,
-          this.filteredPostList.length
-        );
-        console.log(this.filteredPostList);
-      }
-    } else {
-      this.filteredPostList.slice(
-        (pageNumber + 1) * 10 + 1,
-        (pageNumber + 1) * 10 + 11
-      );
-    }
   }
 }
